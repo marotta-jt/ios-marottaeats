3328ff75671ad277840fc52b60344c6f991879fe
Joshua Marotta <marotta.jt@gmail.com>
2026-06-18 13:55:57 -0600

    Initial Commit

---
+++ marottaeatsTests/marottaeatsTests.swift
@@ -0,0 +1,18 @@
+//
+//  marottaeatsTests.swift
+//  marottaeatsTests
+//
+//  Created by Joshua Marotta on 6/18/26.
+//
+
+import Testing
+
+struct marottaeatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ marottaeatsUITests/marottaeatsUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  marottaeatsUITestsLaunchTests.swift
+//  marottaeatsUITests
+//
+//  Created by Joshua Marotta on 6/18/26.
+//
+
+import XCTest
+
+final class marottaeatsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
